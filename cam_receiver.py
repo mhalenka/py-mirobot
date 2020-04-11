10c608f88225452a640852493d78036adada7ec0
Matt <matt@halenka.com>
2020-04-11 14:33:30 -0500

    Network camera.

---
+++ cam_receiver.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import imagezmq
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+args = vars(ap.parse_args())
+
+# initialize the ImageHub object
+imageHub = imagezmq.ImageHub()
+
+# start looping over all the frames
+while True:
+    # receive RPi name and frame from the RPi and acknowledge
+    # the receipt
+    (rpiName, frame) = imageHub.recv_image()
+    imageHub.send_reply(b"OK")
+
+    cv2.imshow(frame)
+    cv2.waitKey(0.01)
